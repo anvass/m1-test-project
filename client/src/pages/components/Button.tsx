68486396d5e2394bd7a729e6238d3d3ae55b7a9f
anvass <nastya_0809@mail.ru>
2025-04-10 21:54:09 +0300

    fix: 'set active' btn

--- client/src/pages/components/Button.tsx
+++ client/src/pages/components/Button.tsx
@@ -1,9 +1,13 @@
 import React, { memo, useCallback } from 'react';
 
 const Button: React.FC<any> = ({ onClick, id, disabled, children }) => {
-  const handleClick = useCallback(() => {
-    onClick(id);
-  }, []);
+  const handleClick = useCallback(
+    (e: React.MouseEvent<HTMLButtonElement>) => {
+      onClick(id);
+      e.preventDefault();
+    },
+    [id]
+  );
 
   return (
     <button onClick={handleClick} disabled={disabled}>
